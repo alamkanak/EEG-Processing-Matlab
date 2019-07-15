f5d32aac07b223b5668164ce85cea71ddf4542d6
rala7323 <braveHook61>
2019-07-15 16:54:39 +1000

    Progress towards resampling

--- CleanEpochsScript.m
+++ CleanEpochsScript.m
@@ -1,4 +1,4 @@
-path = '/Users/alam/Desktop/VisualStudioWorkspaces/mep-classification/data/original/sub01/exp01/eeg/SP 110RMT r3'
-sourceFile = 'Terry_Baedon_2019-02-22_15-26-48.cnt'
+path = '/home/raquib/Desktop/workspaces/mep-classification/data/original/sub03/exp01/eeg/SP 110RMT r1'
+sourceFile = 'Brunet_Sarah_2018-11-29_15-41-19.cnt'
 EEG = CleanEpochs(strcat(path, '/', sourceFile))
 pop_saveset(EEG, 'filename', 'clean.set', 'filepath', path);
