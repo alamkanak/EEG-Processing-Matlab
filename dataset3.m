438bb03aa3f8a271c00273954e18b54710ce2418
rala7323 <braveHook61>
2020-08-12 14:19:04 +1000

    Dataset 3 epochs of 1 sec

--- dataset3.m
+++ dataset3.m
@@ -4,7 +4,7 @@ addpath('functions/')
 addpath('/Users/alam/Documents/MATLAB/eeglab14_1_2b_edited')
 
 %%
-fileList = dir("/Users/alam/Dropbox (Sydney Uni)/Sydney Research/TMS EEG/MATLAB Pipeline/dataset3/original/*.bdf");
+fileList = dir("/home/raquib/Desktop/workspaces/mep-classification/data/dataset3/original/*.bdf");
 % for k = 1:length(fileList)
 file = fileList(1);   
 eeglab
@@ -13,15 +13,15 @@ eeglab
 % Raw EEG.
 EEG = pop_biosig(strcat(file.folder, '/', file.name));
 EEG = eeg_checkset( EEG );
-EEG = eeg_regepochs( EEG, 'recurrence', 2);
+EEG = eeg_regepochs( EEG, 'recurrence', 1);
 EEG = eeg_checkset( EEG );
-EEG = pop_chanedit(EEG, 'lookup', '/Users/alam/Documents/MATLAB/eeglab14_1_2b/plugins/dipfit2.3/standard_BESA/standard-10-5-cap385.elp','load',{'/Users/alam/Dropbox (Sydney Uni)/Sydney Research/TMS EEG/MATLAB Pipeline/dataset3/locations/EEG_Cat_Locs.ced' 'filetype' 'autodetect'});
+EEG = pop_chanedit(EEG, 'lookup', '/home/raquib/Documents/eeglab14_1_1b/plugins/dipfit2.3/standard_BESA/standard-10-5-cap385.elp','load',{'/home/raquib/Desktop/workspaces/mep-classification/data/dataset3/locations/EEG_Cat_Locs.ced' 'filetype' 'autodetect'});
 EEG = eeg_checkset( EEG );
-pop_saveset(EEG, 'filename', strcat(file.name(1:end-4), '.set'), 'filepath', 'dataset3/raw');
+pop_saveset(EEG, 'filename', strcat(file.name(1:end-4), '.set'), 'filepath', '/home/raquib/Desktop/workspaces/mep-classification/data/dataset3/raw');
 
 %%
 % Raw Hjorth.
-SaveHjorth(strcat('dataset3/raw/', file.name(1:end-4), '.set'), strcat('dataset3/raw-hjorth/', file.name(1:end-4), '.mat'));
+SaveHjorth(strcat('dataset3/raw/', file.name(1:end-4), '.set'), strcat('/home/raquib/Desktop/workspaces/mep-classification/data/dataset3/raw-hjorth/', file.name(1:end-4), '.mat'));
 
 %%
 % Clean EEG by running ICA.
@@ -34,9 +34,9 @@ rejected = rejected{45};
 rejected = split(rejected, '  ');
 rejected = cellfun(@str2num, rejected');
 EEG = pop_subcomp( EEG, rejected, 0);
-pop_saveset(EEG, 'filename', strcat(file.name(1:end-4), '.set'), 'filepath', 'dataset3/clean');
+pop_saveset(EEG, 'filename', strcat(file.name(1:end-4), '.set'), 'filepath', '/home/raquib/Desktop/workspaces/mep-classification/data/dataset3/clean');
 
 %%
 % Clean Hjorth.
-SaveHjorth(strcat('dataset3/clean/', file.name(1:end-4), '.set'), strcat('dataset3/clean-hjorth/', file.name(1:end-4), '.mat'));
+SaveHjorth(strcat('dataset3/clean/', file.name(1:end-4), '.set'), strcat('/home/raquib/Desktop/workspaces/mep-classification/data/dataset3/clean-hjorth/', file.name(1:end-4), '.mat'));
 % end
